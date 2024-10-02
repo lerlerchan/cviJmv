a86f22c88d9aa3c17517972255d28e5fb99548d8
lerlerchan <lkchan@sc.edu.my>
2024-10-02 15:33:40 +0800

    update
    update code

--- R/cvi.b.R
+++ R/cvi.b.R
@@ -189,7 +189,7 @@ CVIClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
           #self$results$text$setContent(uaValues)
  
           for (i in 1:nrow(combined_UA_df)) {
-            values <- (combined_UA_df$UaValue[i])
+            values <- list(combined_UA_df$UaValue[i])
             table1$setRow(rowNo = 3, values)
           }
           
